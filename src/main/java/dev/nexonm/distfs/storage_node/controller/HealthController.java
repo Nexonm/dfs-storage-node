d99eebc052056978941ced7327e40c4f8343e2d0
Nexonm <nikita96max@gmail.com>
2025-04-26 22:11:16 +0300

    added: log for healthcheck endpoint

--- src/main/java/dev/nexonm/distfs/storage_node/controller/HealthController.java
+++ src/main/java/dev/nexonm/distfs/storage_node/controller/HealthController.java
@@ -1,5 +1,6 @@
 package dev.nexonm.distfs.storage_node.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/health")
+@Slf4j
 public class HealthController {
 
     @GetMapping("/check")
     public ResponseEntity<String> health() {
+        log.info("Health check requested");
         return ResponseEntity.ok("Healthy");
     }
 }
